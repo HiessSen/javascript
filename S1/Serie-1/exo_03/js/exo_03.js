b3ec9027beb0d06b9046845d6c4afeb8da48edac
HiessSen <118723696+HiessSen@users.noreply.github.com>
2022-12-07 15:01:57 +0100

    JS exos

---
+++ S1/Serie-1/exo_03/js/exo_03.js
@@ -0,0 +1,3 @@
+let temperatureCelcius = 0;
+let temperatureKelvin = temperatureCelcius - 273.15;
+console.log("la temperature de "+ temperatureCelcius + " °Celsius vaut " + temperatureKelvin + " en Kelvin");
