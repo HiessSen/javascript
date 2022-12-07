af5d7db9cd3127242386e23c331f4556a073d888
HiessSen <118723696+HiessSen@users.noreply.github.com>
2022-12-07 10:08:47 +0100

    Update index.js

--- cours/cours_1/index.js
+++ cours/cours_1/index.js
@@ -3,12 +3,10 @@ console.log(rayonCercle);
 let resultat = rayonCercle + 10;
 // resultat : 50 + 10 = 60
 console.log(resultat);
-resultat = rayonCercle -5;
-
+resultat = rayonCercle -5;  
 console.log(resultat);
 // resultat vaut 50 - 5 soit 45;
 resultat = rayonCercle * 2;
-
 console.log(resultat);
 // resultat vaut 50 * 2 soit 100
 resultat = rayonCercle / 10;
@@ -18,9 +16,9 @@ console.log('La tête à toto');
 
 
 let age = "44";
-age +-1 // age = age +1
+age +=1; // age = age +1
 console.log(age);
 let ageEnNombre = parseInt(age);
 ageEnNombre +=1;
 console.log(ageEnNombre);
-console.log(); (parseInt(age));
+console.log(parseInt(age));
