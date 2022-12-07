17c2d73aaf3d6309ef615618310d620f073dcae2
HiessSen <118723696+HiessSen@users.noreply.github.com>
2022-12-07 10:01:12 +0100

    cours 01 JS

---
+++ cours/cours_1/index.js
@@ -0,0 +1,26 @@
+let rayonCercle = 50;
+console.log(rayonCercle);
+let resultat = rayonCercle + 10;
+// resultat : 50 + 10 = 60
+console.log(resultat);
+resultat = rayonCercle -5;
+
+console.log(resultat);
+// resultat vaut 50 - 5 soit 45;
+resultat = rayonCercle * 2;
+
+console.log(resultat);
+// resultat vaut 50 * 2 soit 100
+resultat = rayonCercle / 10;
+console.log(resultat);
+// resultat vaut 50/10 soit 5
+console.log('La tête à toto');
+
+
+let age = "44";
+age +-1 // age = age +1
+console.log(age);
+let ageEnNombre = parseInt(age);
+ageEnNombre +=1;
+console.log(ageEnNombre);
+console.log(); (parseInt(age));
