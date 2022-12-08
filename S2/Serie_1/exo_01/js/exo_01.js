c62c72e0150d0c1c2b839562430c7a43ee99095e
HiessSen <118723696+HiessSen@users.noreply.github.com>
2022-12-08 09:37:41 +0100

    seance 2 js

---
+++ S2/Serie_1/exo_01/js/exo_01.js
@@ -0,0 +1,5 @@
+let prenom = "Ilan";
+let age = 30;
+
+age = parseInt(age + 1);
+console.log("Je m'appelle " + prenom + " et j'aurai bientôt " + age + " ans.");
---
+++ S2/Serie_1/exo_05/js/exo_05.js
@@ -0,0 +1,3 @@
+let rayon = 10;
+let surface =  Math.PI * Math.pow(parseInt(rayon),2);
+console.log("le cercle de rayon " + rayon + " cm pour surface " + surface + " cm2 ");
