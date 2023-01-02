af124820d086885bb02214ad25365d2c88ad6113
HiessSen <118723696+HiessSen@users.noreply.github.com>
2023-01-02 16:27:10 +0100

    sgdsg

--- sequence_2/seance_1/tp1/tp1.js
+++ sequence_2/seance_1/tp1/tp1.js
@@ -1,4 +1,26 @@
-let body = document.body;
+let btnRandom = document.querySelector('#boutonRandom');
+let btnCouleur = document.querySelector('#boutonCouleur');
+let btnAjouterH1 = document.querySelector('#boutonAjouterH1');
+let ilFautAjouterH1 = document.createElement("h1");
 
+function couleurRandom(){
+    document.body.style.backgroundColor = "#0075a2";
+}
 
-document.body.style.backgroundColor='red';
+function boutonCouleur(){
+    if(btnCouleur.style.backgroundColor == "white"){
+        btnCouleur.style.backgroundColor = "black";
+        btnCouleur.style.color = "white";
+    }else{
+        btnCouleur.style.backgroundColor = "white";
+        btnCouleur.style.color = "black";
+    }
+}
+
+function ajouterH1(){
+    ilFautAjouterH1.innerHTML = 'TITRE';
+}
+
+btnRandom.addEventListener('click',couleurRandom);
+btnCouleur.addEventListener('click',boutonCouleur);
+btnAjouterH1.addEventListener('click',ajouterH1)
